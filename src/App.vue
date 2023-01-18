a1e7e408b8f9cdf2a156c9782d6741ba007de1aa
AngeloCalabro <angelo.1991@hotmail.com>
2023-01-18 14:50:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="(project,id) in projects" :key="id">
+        {{ project.name_project }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Hi Vue!',
+      apiURL: 'http://localhost:8000/api',
+      projects: [],
+    }
+  },
+  methods: {
+    getProject() {
+      axios.get(`${this.apiURL}/projects`).then((response) => {
+        // console.log(response)
+        this.projects = response.data.results.data;
+        console.log(this.projects);
+        console.log(response.data);
+      })
+    }
+  },
+  mounted() {
+    this.getProject();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
